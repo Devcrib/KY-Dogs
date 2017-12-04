ea4c95749ff48846bee4a62ebd420f14b43d919f
omolegrace\omole grace <omojesu.ogo@gmail.com>
2017-12-04 15:37:41 +0100

    grace

---
+++ dog.js
@@ -0,0 +1,29 @@
+var input = document.getElementById("dogs");
+
+var main = () =>{
+	var awesomplete = new Awesomplete(input, {
+		minChars: 1,
+		//maxItems:
+		autoFirst: true
+
+	});
+	awesomplete.list = ["affenpinscher","african","airedale","akita","appenzeller","basenji","beagle","bluetick","borzoi","bouvier","boxer","brabancon","briard","bulldog","bullterrier","cairn","chihuahua","chow","clumber","collie","coonhound","corgi","dachshund","dane","deerhound","dhole","dingo","doberman","elkhound","entlebucher","eskimo","germanshepherd","greyhound","groenendael","hound","husky","keeshond","kelpie","komondor","kuvasz","labrador","leonberg","lhasa","malamute","malinois","maltese","mastiff","mexicanhairless","mountain","newfoundland","otterhound","papillon","pekinese","pembroke","pinscher","pointer","pomeranian","poodle","pug","pyrenees","redbone","retriever","ridgeback","rottweiler","saluki","samoyed","schipperke","schnauzer","setter","sheepdog","shiba","shihtzu","spaniel","springer","stbernard","terrier","vizsla","weimaraner","whippet","wolfhound"];
+
+};
+main();
+
+
+//$("#dogs").on("keyup", function(){
+//  $.ajax({
+//    url: 'https://dog.ceo/api/breeds/list' + this.value,
+//    type: 'GET',
+//    dataType: 'json'
+//  })
+//  .success(function(data) {
+//    var list = [];
+//    $.each(data, function(key, value) {
+//      list.push(value.name);
+//    });
+//    awesomplete.list = list;
+//  });
+//});
